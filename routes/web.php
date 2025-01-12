247c26d4da4a27158f0a735b552743d69d7fc395
abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-01-12 14:10:55 +0200

    Refactor routes to use HomeController for the homepage and create a custom 404 error view.

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/user/{id}', function (string $id) {
-    echo "profile: id = {$id}";
-})->name('profile');
 
-Route::get('/test/{id}', function (string $id) {
-    return redirect()->route('profile', ['id' => $id]); // Example with id = 1
-});
-Route::get('/there', function () {
-    echo "we are here!";
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::match(['get', 'post'], '/hi', function () {
-    dump($_SERVER['REQUEST_METHOD']);
-});
-Route::redirect('/here', '/there');
-Route::get('/search/{search}', function (string $search) {
-    foreach (explode(' ', $search) as $word) {
-        echo "search: {$word}<br>";
-    }
-})->where('search', '.+');
-Route::get
-('/watch/{VId}', function (string $videoId) {
-    echo "watch: {$videoId}";
-});
-
-Route::prefix('/admin')->group(function () {
-    Route::get('/users', function () {
-        echo "admin users";
-    });
-});
-Route::name('admin.')->group(function () {
-    Route::get('/users', function () {
-        echo "here admin.users";
-    })->name('users');
-});
 
 Route::fallback(function () {
     return view('404');
