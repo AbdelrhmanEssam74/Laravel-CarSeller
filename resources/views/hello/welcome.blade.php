f502e427f80d3f94e39b1b76f6c0e8995a9c40f9
abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-01-14 15:05:30 +0200

    Remove index view and update routing to welcome page

---
+++ resources/views/hello/welcome.blade.php
@@ -0,0 +1,2 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<p class="bg-red-500 p-4 text-center text-white text-3xl">{{ Str::after('hello world', 'hello') }}</p>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/hello', [HomeController::class, 'welcome']);
 
 
 Route::fallback(function () {
