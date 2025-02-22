3063155a785d52eca3aee35563ae21d7fa95d5ef
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-02-22 08:23:11 +0200

    update sing up page with new design

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,61 +1,55 @@
 @extends('layouts.app')
 @section('title', 'Sign Up')
 @section('content')
-    <div class="page-signup">
-        <main>
-            <div class="container-small page-login">
-                <div class="flex" style="gap: 5rem">
-                    <div class="auth-page-form">
-                        <div class="text-center">
-                            <a href="/">
-                                <img src="/img/logoipsum-265.svg" alt="" />
-                            </a>
-                        </div>
-                        <h1 class="auth-page-title">Signup</h1>
-
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <input type="email" placeholder="Your Email" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" placeholder="Your Password" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" placeholder="Repeat Password" />
-                            </div>
-                            <hr />
-                            <div class="form-group">
-                                <input type="text" placeholder="First Name" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" placeholder="Last Name" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" placeholder="Phone" />
-                            </div>
-                            <button class="btn btn-primary btn-login w-full">Register</button>
-
-                            <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-                                <button class="btn btn-default flex justify-center items-center gap-1">
-                                    <img src="/img/google.png" alt="" style="width: 20px" />
-                                    Google
-                                </button>
-                                <button class="btn btn-default flex justify-center items-center gap-1">
-                                    <img src="/img/facebook.png" alt="" style="width: 20px" />
-                                    Facebook
-                                </button>
-                            </div>
-                            <div class="login-text-dont-have-account">
-                                Already have an account? -
-                                <a href="/login"> Click here to login </a>
-                            </div>
-                        </form>
+    <!-- component -->
+    <div class="flex h-screen">
+        <div class="w-full bg-gray-100 lg:w-1/2 flex items-center justify-center">
+            <div class="max-w-md w-full p-6">
+                <h1 class="text-3xl font-semibold mb-6 text-black text-center">Sign Up</h1>
+                <h1 class="text-sm font-semibold mb-6 text-gray-500 text-center">Join to Our Community with all time access and free </h1>
+                <div class="mt-4 flex flex-col lg:flex-row items-center justify-between">
+                    <div class="w-full lg:w-1/2 mb-2 lg:mb-0">
+                        <button type="button" class="w-full flex justify-center items-center gap-2 bg-white text-sm text-gray-600 p-2 rounded-md hover:bg-gray-50 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition-colors duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4" id="google">
+                                <path fill="#fbbb00" d="M113.47 309.408 95.648 375.94l-65.139 1.378C11.042 341.211 0 299.9 0 256c0-42.451 10.324-82.483 28.624-117.732h.014L86.63 148.9l25.404 57.644c-5.317 15.501-8.215 32.141-8.215 49.456.002 18.792 3.406 36.797 9.651 53.408z"></path>
+                                <path fill="#518ef8" d="M507.527 208.176C510.467 223.662 512 239.655 512 256c0 18.328-1.927 36.206-5.598 53.451-12.462 58.683-45.025 109.925-90.134 146.187l-.014-.014-73.044-3.727-10.338-64.535c29.932-17.554 53.324-45.025 65.646-77.911h-136.89V208.176h245.899z"></path>
+                                <path fill="#28b446" d="m416.253 455.624.014.014C372.396 490.901 316.666 512 256 512c-97.491 0-182.252-54.491-225.491-134.681l82.961-67.91c21.619 57.698 77.278 98.771 142.53 98.771 28.047 0 54.323-7.582 76.87-20.818l83.383 68.262z"></path>
+                                <path fill="#f14336" d="m419.404 58.936-82.933 67.896C313.136 112.246 285.552 103.82 256 103.82c-66.729 0-123.429 42.957-143.965 102.724l-83.397-68.276h-.014C71.23 56.123 157.06 0 256 0c62.115 0 119.068 22.126 163.404 58.936z"></path>
+                            </svg> Sign Up with Google </button>
                     </div>
-                    <div class="auth-page-image">
-                        <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
+                    <div class="w-full lg:w-1/2 ml-0 lg:ml-2">
+                        <button type="button" class="w-full flex justify-center items-center gap-2 bg-white text-sm text-gray-600 p-2 rounded-md hover:bg-gray-50 border border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 transition-colors duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                            </svg> Sign Up with Facebook </button>
                     </div>
                 </div>
+                <div class="mt-4 text-sm text-gray-600 text-center">
+                    <p>or with email</p>
+                </div>
+                <form action="#" method="POST" class="space-y-4">
+                    <!-- Your form elements go here -->
+                    <div>
+                        <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                        <input type="text" id="username" name="username" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+                    </div>
+                    <div>
+                        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                        <input type="email" id="email" name="email" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+                    </div>
+                    <div>
+                        <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                        <input type="password" id="password" name="password" class="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+                    </div>
+                    <div>
+                        <button type="submit" class="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Sign Up</button>
+                    </div>
+                </form>
+                <div class="mt-4 text-sm text-gray-600 text-center">
+                    <p>Already have an account? <a href="/login" class="text-black hover:underline">Login here</a>
+                    </p>
+                </div>
             </div>
-        </main>
+        </div>
     </div>
 @endsection
