2bcb9b7580eb681cd7a5cfc28e8b52fa4104a283
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-02-22 09:32:38 +0200

    refactor routing to use dedicated controllers for login and signup

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginControllers;
+use App\Http\Controllers\SignupControllers;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'welcome']);
 Route::get('/about', [HomeController::class, 'about']);
-Route::get('/login', [HomeController::class, 'login']);
-Route::get('/signup', [HomeController::class, 'signup']);
+Route::get('/login', [LoginControllers::class, 'create']);
+Route::get('/signup', [SignupControllers::class, 'create']);
 Route::redirect('/home', '/');
 
 Route::fallback(function () {
