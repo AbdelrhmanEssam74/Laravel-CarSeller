c7b3fefe878dcded0bc3e2232e4f090c5f5b7769
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-02-22 08:44:34 +0200

    refactor layout structure by creating a clean template and updating app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+@extends('layouts.clean')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>@yield('title') | {{ config('app.name') }}</title>
-</head>
-
-<body>
-@yield('header')
+@section('childContent')
     @yield('content')
-</body>
-
-</html>
+@endsection
---
+++ resources/views/layouts/clean.blade.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>@yield('title') | {{ config('app.name') }}</title>
+</head>
+
+<body>
+
+@yield('childContent')
+
+</body>
+
+</html>
